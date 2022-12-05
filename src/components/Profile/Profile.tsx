a1c9be4706d2030cac62be8d1f56fd1f58fe5997
Ivan Shirykhalov <ivanshirykhalov@gmail.com>
2022-12-05 21:46:55 +0300

    dialogsComponent

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div /*className={s.content}*/>
             <div><img src="https://cdn.jpegmini.com/user/images/bullet-1-mobile.jpg" alt="Profile logo"/></div>
             <div>avatar + description</div>
             <MyPosts/>
